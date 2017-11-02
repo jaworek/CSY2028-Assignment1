df63bf9a677af3f15da8d7444f76b492d35b1b9d
jaworek <jaworek3211@gmail.com>
2017-11-02 10:24:57 +0000

    Add content specific for home page

--- public/index.php
+++ public/index.php
@@ -1,3 +1,41 @@
 <?php
 $title = "Home";
+$content = '
+<main>
+
+  <nav>
+    <ul>
+      <li><a href="#">Sidebar</a></li>
+      <li><a href="#">This can</a></li>
+      <li><a href="#">Be removed</a></li>
+      <li><a href="#">When not needed</a></li>
+    </ul>
+  </nav>
+
+  <article>
+    <h2>A Page Heading</h2>
+    <p>Text goes in paragraphs</p>
+
+    <ul>
+      <li>This is a list</li>
+      <li>With multiple</li>
+      <li>List items</li>
+    </ul>
+
+
+    <form>
+      <p>Forms are styled like so:</p>
+
+      <label>Field 1</label>
+      <input type="text">
+      <label>Field 2</label>
+      <input type="text">
+      <label>Textarea</label>
+      <textarea></textarea>
+
+      <input type="submit" name="submit" value="Submit" />
+    </form>
+  </article>
+</main>
+';
 require '../layout.php';
